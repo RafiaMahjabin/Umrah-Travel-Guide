6352bf2b3c2aee16c14dafc5017a88db3dd3a7f3
Your Name <you@example.com>
2025-02-03 13:42:56 +0600

    umrah guide

---
+++ emergrncy contact/emergencyContact.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Emergency Contacts</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css">
+    <link rel="stylesheet" href="../css/contact.css">
+</head>
+<body>
+    <div class="container">
+        <a href="../View/index.php" class="home-icon"><i class="fas fa-home"></i></a>
+        <h1>Emergency Contacts</h1>
+        <div class="info-container">
+            <div class="info-box">
+                <i class="fas fa-phone"></i>
+                <h3>Police</h3>
+                <p>Mecca: +966 12 542 5555</p>
+                <p>Medina: +966 14 822 5555</p>
+                <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.902123456789!2d39.826168314932!3d21.422510985591!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x15c204b1b1b1b1b1%3A0x1b1b1b1b1b1b1b1b!2sMecca%20Police%20Station!5e0!3m2!1sen!2ssa!4v1603794290143!5m2!1sen!2ssa" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+            </div>
+            <div class="info-box">
+                <i class="fas fa-hospital"></i>
+                <h3>Hospital</h3>
+                <p>Mecca: +966 12 550 4444</p>
+                <p>Medina: +966 14 822 4444</p>
+                <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.902123456789!2d39.826168314932!3d21.422510985591!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x15c204b1b1b1b1b1%3A0x1b1b1b1b1b1b1b1b!2sMecca%20Hospital!5e0!3m2!1sen!2ssa!4v1603794290143!5m2!1sen!2ssa" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+            </div>
+            <div class="info-box">
+                <i class="fas fa-flag"></i>
+                <h3>Embassy</h3>
+                <p>Mecca: +966 12 550 3333</p>
+                <p>Medina: +966 14 822 3333</p>
+                <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.902123456789!2d39.826168314932!3d21.422510985591!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x15c204b1b1b1b1b1%3A0x1b1b1b1b1b1b1b1b!2sMecca%20Embassy!5e0!3m2!1sen!2ssa!4v1603794290143!5m2!1sen!2ssa" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+            </div>
+        </div>
+        <a href="contact.php" class="back-icon"><i class="fas fa-arrow-left"></i></a>
+    </div>
+</body>
+</html>
